9ef943f2c2fc6f3251487c2c3af1cdafde6b326a
sinequanonn <dleck288@gmail.com>
2025-10-16 00:32:42 +0900

    feat: 덧셈 게산기 메인 로직 구현
    - 입력, 숫자 추출, 계산, 출력 연결
    - Application에서 컴포넌트 통합

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        NumberExtractor numberExtractor = new NumberExtractor();
+        Calculator calculation = new Calculator();
+
+        String inputString = inputView.input();
+        List<Integer> numbers = numberExtractor.extract(inputString);
+        Integer sum = calculation.sum(numbers);
+        outputView.printResult(sum);
     }
 }
