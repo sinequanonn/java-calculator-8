7b0674c3473cc3d8187df1dc4d43e23c29400e58
sinequanonn <dleck288@gmail.com>
2025-10-18 23:53:32 +0900

    refactor: 정수인 경우 소숫점 없이 출력하도록 변경

--- src/main/java/calculator/OutputView.java
+++ src/main/java/calculator/OutputView.java
@@ -5,6 +5,16 @@ public class OutputView {
     private static final String RESULT_MESSAGE = "결과 : ";
 
     public void printResult(Double sum) {
+
+        if (isInteger(sum)) {
+            long intSum = sum.longValue();
+            System.out.println(RESULT_MESSAGE + intSum);
+            return;
+        }
         System.out.println(RESULT_MESSAGE + sum);
+    }
+
+    private boolean isInteger(Double sum) {
+        return sum % 1 == 0;
     }
 }
